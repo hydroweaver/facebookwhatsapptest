c1386e61b4afd292c858f0b595f74aeda15d597b
Karan Verma <hydroweaver@gmail.com>
2022-12-06 18:15:30 +0530

    port fixed in index

--- index_hi_qeenu_template.js
+++ index_hi_qeenu_template.js
@@ -18,8 +18,8 @@ app
     console.log(req.query["hub.verify_token"]);
     res.send(req.query["hub.challenge"]);
   })
-  .listen(3000, () => {
-    console.log("Listening at 3000");
+  .listen(process.env.PORT, () => {
+    console.log(`Listening at ${process.env.PORT}`);
   });
 
 const headers = {
