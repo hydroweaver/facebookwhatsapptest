d6f5c045f7de310bae843764f2ef2f3869383982
Karan Verma <hydroweaver@gmail.com>
2022-12-06 18:06:42 +0530

    pushing to fly.io

--- index_hi_qeenu_template.js
+++ index_hi_qeenu_template.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.get('/', (req, res)=>{
+  res.send('Hello World!')
+})
+
 app
   .get("/webhook", (req, res) => {
     console.log(req.url);
